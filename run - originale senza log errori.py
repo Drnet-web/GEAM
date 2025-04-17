148161e36ffba4336f8e1db54024a3b6009873a7
Drnet-web <drnetoutlook.it>
2025-04-17 21:49:42 +0200

    Rilascio v2.5.13

--- run - originale senza log errori.py
+++ run - originale senza log errori.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 from app import create_app
-import sys
-
-# Log su file per NSSM
-sys.stdout = open("log.txt", "w")
-sys.stderr = open("err.txt", "w")
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
+    
+    import sys
